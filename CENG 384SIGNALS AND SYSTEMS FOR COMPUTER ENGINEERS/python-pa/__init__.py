da72de369f8f499352340a5b18710e1427cde1fc
Zeki Oren <orenzeki@gmail.com>
2019-07-06 16:51:03 +0300

    Add files via upload

---
+++ CENG 384SIGNALS AND SYSTEMS FOR COMPUTER ENGINEERS/python-pa/__init__.py
@@ -0,0 +1,14 @@
+# Some setup utils
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+
+%matplotlib inline
+plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+# for auto-reloading external modules
+# see http://stackoverflow.com/questions/1907993/autoreload-of-modules-in-ipython
+%load_ext autoreload
+%autoreload 2
